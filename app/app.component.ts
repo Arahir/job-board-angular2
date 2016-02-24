108a2a9202b59096a9178a314f11bf1cb762d259
matthieu faugere <matthieu.faugere@gmail.com>
2016-02-24 10:29:38 +0100

    Display a job

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,27 @@
 import {Component} from 'angular2/core';
 
+interface Job {
+  id: number;
+  name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+      <h1>{{title}}</h1>
+      <h2>{{job.name}} details!</h2>
+      <div>
+        <label>id: </label>{{job.id}}
+      </div>
+      <div>
+        <label>name: </label>
+        <div><input [(ngModel)]="job.name" placeholder="name"></div>
+      </div>`
 })
-export class AppComponent { }
+export class AppComponent {
+  public title = 'List of jobs';
+  public job : Job = {
+    id: 1,
+    name: 'Front End Developer'
+  };
+}
